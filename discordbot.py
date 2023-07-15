6507d52d45db2af1cc96a7efbdba1dcfaaaf22ac
ultimatemagic79 <sea.starry.ks@gmail.com>
2023-07-15 18:43:45 +0900

    feat: add discordbot

---
+++ discordbot.py
@@ -0,0 +1,34 @@
+# インストールした discord.py を読み込む
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print("ログインしました")
+
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == "/league":
+        await message.channel.send("リーグの結果が返ってくるようにします！")
+
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
